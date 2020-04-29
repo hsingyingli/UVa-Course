9cbd5488e5579025786571b68ed64eac0f0d5200
Hsing-Ying Li <yy8secviielnce@gmail.com>
2020-04-29 14:11:02 +0800

    2020

--- uva12124.cpp
+++ uva12124.cpp
@@ -3,37 +3,80 @@
 #include<iostream>
 #include<vector>
 #include<string>
+#include<algorithm>
+#include<map>
+#include<cstdio>
 using namespace std;
 
-struct component
+struct Component
 {
     int price;
     int quality;
 
 };
-vector<component> comp;
-vector<int>id;
+int cnt,n,budget;
+
+map<string, int>id;
+int ID(string s)
+{
+    if(!id.count(s))
+        id[s] = cnt++;
+    return id[s];
+}
+
+vector<Component>comp[1005];
+
+bool ok(int q)
+{
+    int sum = 0;
+    for(int i = 0;i<cnt;i++)
+    {
+        int cheapest = budget+1, m = comp[i].size();
+        for(int j =0;j<m;j++)
+        {
+            if(comp[i][j].quality>=q)
+            {   
+                cheapest = min(cheapest,comp[i][j].price);
+            }
+        }
+        if(cheapest == budget+1) return false;
+        sum += cheapest;
+        if(sum >budget) return false;
+    }
+    return true;
+}
 int main()
 {
-    freopen("12124.in","r",stdin);
-    
+    freopen("./database/12124.in","r",stdin);
     int cases = 0;
     scanf("%d",&cases);
+    cout << cases << "\n";
     while(cases--)
     {
-        int n , budget,p,q;
-        string type,name,buffer;
         scanf("%d %d",&n,&budget);
         for(int i = 0;i<n;i++)
         {
-            scanf("%s%s%d%d",type,name,&p,&q);
-            if(buffer != type)
-                id.push_back(i);
-            buffer = type;
+            comp[i].clear();
         }
+        id.clear();
 
-        
-
+        int max_q = 0;
+        for(int i = 0;i<n;i++)
+        {
+            char type[30] ,name[30];
+            int p,q;
+            scanf("%s%s%d%d",type,name,&p,&q);
+            max_q = max(max_q,q);
+            comp[ID(type)].push_back(Component{p,q});
+        }
+        int L = 0,R = max_q;
+        while(L<R)
+        {
+            int mid = (L+R+1)/2;
+            if(ok(mid)) L = mid;
+            else R = mid-1;
+        }
+        cout <<L;
 
     }
     return 0;
